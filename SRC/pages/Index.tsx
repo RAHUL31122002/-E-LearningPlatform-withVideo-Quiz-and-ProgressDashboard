52b609202c6d0dee9cc0781a0313a42ffea191a7
RAHUL KUMAR <rahul31122002@gmail.com>
2025-09-11 10:39:16 +0530

    Create Index.tsx

---
+++ SRC/pages/Index.tsx
@@ -0,0 +1,23 @@
+import Header from "../components/Header";
+import HeroSection from "../components/HeroSection";
+import CoursesSection from "../components/CoursesSection";
+import FeaturesSection from "../components/FeaturesSection";
+import CTASection from "../components/CTASection";
+import Footer from "../components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Header />
+      <main>
+        <HeroSection />
+        <CoursesSection />
+        <FeaturesSection />
+        <CTASection />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
